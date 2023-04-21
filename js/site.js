8f5e594bd06d4f5529c08140bcd789d0b4f497d6
bryantgates <bryantmgates@gmail.com>
2023-04-21 17:54:37 -0400

    everythong

--- js/site.js
+++ js/site.js
@@ -1,10 +1,99 @@
-function displayMessage() {
-  let msg = document.getElementById("message").value;
-//   alert(msg);
+function getInputs() {
+  let loan = document.getElementById("loan").value;
+  let term = document.getElementById("term").value;
+  let rate = document.getElementById("rate").value;
 
-  Swal.fire({
-    backdrop: false,
-    title: "Appname",
-    text: msg,
-  });
+  let loanAmount = parseFloat(loan);
+  let termAmount = parseFloat(term);
+  let rateAmount = parseFloat(rate);
+
+  let inputs = {
+    loanAmount: loanAmount,
+    termAmount: termAmount,
+    rateAmount: rateAmount,
+  };
+
+  calculateInputs(inputs);
+}
+
+function calculateInputs(inputs) {
+  let paymentArray = [];
+  let totalArray = [];
+
+  let loanAmount = inputs.loanAmount;
+  let termAmount = inputs.termAmount;
+  let rateAmount = inputs.rateAmount;
+
+  //month 1 is loan amount w/out interest. month two - what went to principle
+  let totalMonthlyPayment =
+    (loanAmount * (rateAmount / 1200)) /
+    (1 - (1 + rateAmount / 1200) ** -termAmount);
+  let remainingBalance = loanAmount;
+  let totalInterest = 0;
+  let totalPrincipal = 0;
+  let totalCost = 0;
+
+  for (let i = 1; i <= termAmount; i++) {
+    let interestPayment = remainingBalance * (rateAmount / 1200);
+    let principalPayment = totalMonthlyPayment - interestPayment;
+    remainingBalance -= principalPayment;
+    totalInterest += interestPayment;
+    totalPrincipal += principalPayment;
+    totalCost += totalInterest + totalPrincipal;
+
+    let paymentValues = {
+      month: i,
+      payment: totalMonthlyPayment.toFixed(2),
+      principal: principalPayment.toFixed(2),
+      interest: interestPayment.toFixed(2),
+      totalInterest: totalInterest.toFixed(2),
+      balance: remainingBalance.toFixed(2),
+    };
+    let totalValues = {
+      totalPrinciple: totalPrincipal.toFixed(2),
+      totalInterest: totalInterest.toFixed(2),
+      totalCost: totalCost.toFixed(2),
+    };
+    paymentArray.push(paymentValues);
+    totalArray.push(totalValues);
+  }
+  displayPaymentsTable(paymentArray);
+  displayTotals(totalArray);
+}
+
+function displayPaymentsTable(paymentArray) {
+  const eventTable = document.getElementById("paymentTable");
+  const template = document.getElementById("tableRowTemplate");
+
+  paymentTable.innerHTML = "";
+
+  for (let i = 0; i < paymentArray.length; i++) {
+    let array = paymentArray[i];
+
+    let tableRow = document.importNode(template.content, true);
+
+    tableRow.querySelector('[data-id="month"]').textContent = array.month;
+    tableRow.querySelector('[data-id="payment"]').textContent = array.payment;
+    tableRow.querySelector('[data-id="principal"]').textContent =
+      array.principal;
+    tableRow.querySelector('[data-id="interest"]').textContent = array.interest;
+    tableRow.querySelector('[data-id="totalInterest"]').textContent =
+      array.totalInterest;
+    tableRow.querySelector('[data-id="balance"]').textContent =
+      array.balance;
+
+    tableRow.querySelector("tr").setAttribute("data-array", array.id);
+
+    paymentTable.appendChild(tableRow);
+}
+}
+
+function displayTotals(totalArray) {
+  let principal = totalArray.totalPrinciple;
+  let interest = totalArray.totalInterest;
+  let cost = totalArray.totalCost;
+
+ document.getElementById("totalPrincipal").textContent = principal;
+ document.getElementById("totalInterest").textContent = interest;
+ document.getElementById("totalCost").textContent = cost;
 }
